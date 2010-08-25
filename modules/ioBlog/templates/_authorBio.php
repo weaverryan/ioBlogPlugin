b0fd4a2ce5e0b6e94573a93ca4f8a19bd623e0cc
Ryan Weaver <ryan@thatsquality.com>
2010-08-24 21:58:50 -0500

    Removing the bio portion of the author bio as this requires a Profile model with a bio column. I'm not sure what the author bio should look like yet - probably just added by the developer.

--- modules/ioBlog/templates/_authorBio.php
+++ modules/ioBlog/templates/_authorBio.php
@@ -1,16 +1,15 @@
 <?php use_helper('Gravatar') ?>
 <?php use_stylesheet('/ioBlogPlugin/css/author.css') ?>
 
-<?php if ($blog['Author']['Profile']->bio): ?>
+<?php if ($blog['Author']): ?>
   <div class="author_bio">
     <?php echo image_tag(
       gravatar_image_path($blog['Author']->email_address, 80),
-      array('alt' => $blog['Author']->getFullName(), 'class' => 'float_left')
+      array(
+        'alt' => $blog['Author']->getFirstName().' '.$blog['Author']->getLastName(),
+        'class' => 'float_left'
+      )
     ) ?>
-
-    <p>
-      <b>About the author</b> - <?php echo $blog['Author']['Profile']->getRaw('bio') ?>
-    </p>
 
     <div class="clear">&nbsp;</div>
   </div>
