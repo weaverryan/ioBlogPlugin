c2b81a601900e5fd8500a1385113e668892e7857
Ryan Weaver <ryan@thatsquality.com>
2010-08-29 21:24:04 -0500

    Adding styling to the byline

--- modules/ioBlog/templates/showSuccess.php
+++ modules/ioBlog/templates/showSuccess.php
@@ -1,3 +1,5 @@
+<?php use_stylesheet('/ioBlogPlugin/css/blog.css') ?>
+
 <?php echo editable_content_tag('h1', $blog, 'title') ?>
 
 <?php echo editable_content_tag('div', $blog, 'author_id', array('class' => 'byline', 'method' => 'getAuthorName')) ?>
